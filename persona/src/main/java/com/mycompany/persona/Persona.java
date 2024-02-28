b00e51e6f679d426184785270e51d54ac7f23dbc
Elbarto888 <herreraramosesebas@gmail.com>
2024-02-28 08:49:22 -0500

    Add files via upload

---
+++ persona/src/main/java/com/mycompany/persona/Persona.java
@@ -0,0 +1,68 @@
+
+package com.mycompany.persona;
+
+public class Persona {
+private String nombre;
+private String apellido;
+private String correo;
+private int cedula;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the cedula
+     */
+    public int getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+       
+   
+}
---
+++ persona/src/main/java/com/mycompany/persona/personas.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.persona;
+import static javax.swing.text.html.HTML.Tag.P;
+public class personas {
+    public static void main(String[] args) {
+        Persona p= new Persona();
+        p.setNombre("sebastian herrera");
+        System.out.print(p.getNombre());
+    }
+    
+}
